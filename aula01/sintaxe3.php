24cf5062dfd37934e96af111f849456dea567b09
estherramoss <estherramosbocchini@gmail.com>
2023-08-10 13:32:41 -0300

    aula01

---
+++ aula01/sintaxe3.php
@@ -0,0 +1,12 @@
+<?php
+
+echo                                    "testanto espaço em branco <br>";
+echo "testanto espaço <br>";
+
+// O caracter &nbsp; adiciona um espaço em branco dentro da string
+echo "&#160; &#160                           testando      espaço     em     branco <br>";
+echo "quebra de linha <br>"
+;
+
+echo "quebra de linha <br>"
+;
