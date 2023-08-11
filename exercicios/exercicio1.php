15b70cc6863279f2887ebf95380204d0f576b9bf
estherramoss <estherramosbocchini@gmail.com>
2023-08-11 13:03:38 -0300

    l

---
+++ exercicios/exercicio1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sintaxe Geral</title>
+
+    <!-- CSS Interno -->
+    <style>
+        .destaque {color: red;}
+    </style>
+</head>
+<body>
+    <h1 class="destaque">Trabalhando com PHP</h1>
+    <p>Programador: Esther Bocchini</p>
+    <hr>
+
+    <?php
+    //Geração de texto (String)
+    echo "Chama Sesi-Senai !";
+
+    /* Geração de texto estruturado (com tags, atributos) */
+    echo "<h2>Gerando <span class='destaque' > HTML</span> através do PHP. </h2>"; 
+    echo "<h2>Gerando <span class='destaque' > HTML</span> através do PHP. </h2>"; 
+
+    ?>
+
+    <h1>HTML e PHP mesclados</h1>
+    <p>Parágrafos</p>
+
+    <?php
+      $linguagem = "Linguagem PHP";
+    ?>
+    <p>Parágrafo mesclando HTML com <?=$linguagem?> </p>
+    
+</body>
+</html>
